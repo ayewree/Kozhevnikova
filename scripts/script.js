3121fe1659575bea8e0d53aefb528603a595c3ca
ayewree <emilia.volfson@gmail.com>
2025-06-27 20:37:47 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+"use script"
+
+// Задание 3.2, 3.3
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.intensive__img');
+    
+    images.forEach(img => {
+        const description = img.nextElementSibling;
+        
+        img.addEventListener('mouseenter', () => {
+            img.style.opacity = 0.7;
+            description.style.opacity = 1;
+            description.removeAttribute('hidden');
+        });
+        
+        img.addEventListener('mouseleave', () => {
+            img.style.opacity = 1;
+            description.style.opacity = 0;
+            setTimeout(() => description.setAttribute('hidden', true), 300);
+        });
+    });
+});
